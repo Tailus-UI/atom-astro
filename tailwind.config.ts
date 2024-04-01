dc866041745683015667feb5746895e5a8c4e75a
Meschac Irung <meschacirung@gmail.com>
2024-04-01 18:34:20 +0200

    build speed comparaison section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ module.exports = {
     plugins: [
         themer({
             palette: {
-                extend: "romance",
+                extend: "oz",
             },
             radius: "smooth",
             background: "light",
